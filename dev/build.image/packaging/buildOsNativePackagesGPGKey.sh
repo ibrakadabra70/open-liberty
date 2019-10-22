9296ab84ce54bd656a5f9e98f693a3f5a24ca500
root <root@genetic1.fyre.ibm.com>
2019-10-22 12:45:48 -0700

    fixed building commands

--- dev/build.image/packaging/buildOsNativePackagesGPGKey.sh
+++ dev/build.image/packaging/buildOsNativePackagesGPGKey.sh
@@ -7,17 +7,17 @@ cd rpmbuild && rpmbuild -ba SPECS/openliberty.spec
 if [ -z "$GPG_PASS" ]
 then
      echo "GPG PASSPHRASE is undefined.  skip signing of rpm/deb packages"
-     cd ../debuild/openliberty/debian && debuild -d -b -us -uc
+     cd ../debuild && debuild -d -b -us -uc
 else 
      #!/bin/sh
      #run commands to build the deb 
      echo "${GPG_PASS}" > ~/.gnupg/pp.txt
      if gpg -k |grep admin ; then
          echo gpg key imported correctly
-	 cd ../debuild/openliberty/debian && debuild -d -b -p'gpg --passphrase-file ~/.gnupg/pp.txt --batch'  -e"admin@openliberty.io"
+	 cd ../debuild && debuild -d -b -p'gpg --passphrase-file ~/.gnupg/pp.txt --batch'  -e"admin@openliberty.io"
      else
          echo gpg key not imported correctly
-         cd ../debuild/openliberty/debian && debuild -d -b -us -uc
+         cd ../debuild && debuild -d -b -us -uc
      fi
      rm -f ~/.gnupg/pp.txt
 fi
